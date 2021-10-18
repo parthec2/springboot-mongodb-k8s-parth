609a30b17bddb9ceeab04a30dd15ed23c6e5c4a0
Parth Shah <parthec2@users.noreply.github.com>
2021-10-18 13:46:34 +0530

    Update Client.java

--- src/main/java/com/javatechie/spring/mongo/api/models/Client.java
+++ src/main/java/com/javatechie/spring/mongo/api/models/Client.java
@@ -4,13 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
 @Document(collection="client")
 public class Client {
 
@@ -21,6 +15,44 @@ public class Client {
 	private Integer clientID;
 	private Integer gfcid;
 	private String clientRegion;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public String getClientType() {
+		return clientType;
+	}
+	public void setClientType(String clientType) {
+		this.clientType = clientType;
+	}
+	public Integer getClientID() {
+		return clientID;
+	}
+	public void setClientID(Integer clientID) {
+		this.clientID = clientID;
+	}
+	public Integer getGfcid() {
+		return gfcid;
+	}
+	public void setGfcid(Integer gfcid) {
+		this.gfcid = gfcid;
+	}
+	public String getClientRegion() {
+		return clientRegion;
+	}
+	public void setClientRegion(String clientRegion) {
+		this.clientRegion = clientRegion;
+	}
+	
 	
 	
 }

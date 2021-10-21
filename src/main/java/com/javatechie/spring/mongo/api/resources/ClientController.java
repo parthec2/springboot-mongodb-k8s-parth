860a7d9e12de7e2e4f92d0b24b7cc632c368d554
Parth Shah <parthec2@users.noreply.github.com>
2021-10-21 15:40:57 +0530

    add crossorigin and requestmapping annotation

--- src/main/java/com/javatechie/spring/mongo/api/resources/ClientController.java
+++ src/main/java/com/javatechie/spring/mongo/api/resources/ClientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javatechie.spring.mongo.api.models.Client;
 import com.javatechie.spring.mongo.api.repository.ClientRepo;
 
+@CrossOrigin
+@RequestMapping("/api")
 @RestController
 public class ClientController {
 
